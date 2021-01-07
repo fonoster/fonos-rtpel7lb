2699645aa241e78d85ceb93e4236cc4497155fda
Hector Ventura <hectorvent@gmail.com>
2021-01-06 22:35:04 -0600

    Started adding redis store option

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Server from './Server'
 import { MemoryStore, RedisStore } from './store'
 
 const NG_PORT = process.env.RTPEL7LB_NG_PORT || 22222
-const USE_REDIS_STORE = 'YES' == process.env.RTPEL7LB_STORE_REDIS
+const USE_REDIS_STORE = ('YES' == process.env.RTPEL7LB_REDIS_ENABLED)
 
 // TODO:
 //  - Load API
@@ -12,7 +12,8 @@ let server = new Server(NG_PORT, "0.0.0.0");
 let storeProvider = new MemoryStore();
 
 if (USE_REDIS_STORE){
-  storeProvider = new RedisStore();
+  REDIS_URL = process.env.RTPEL7LB_REDIS_URL;
+  storeProvider = new RedisStore(REDIS_URL);
 }
 
 server.setStoreProvider(storeProvider)
