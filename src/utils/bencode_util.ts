78b5e1859d70ba4074f79020c0de6d49fd00aa39
Pedro Sanders <sanderspedro@gmail.com>
2021-01-29 17:34:51 -0500

    Fixed bencode util

--- src/utils/bencode_util.ts
+++ src/utils/bencode_util.ts
@@ -2,23 +2,22 @@ import bencode from 'bencode'
 
 export const BencodeUtils = {
 
-    decodeMessage: function(msg: string) {
+    decodeMessage: (msg: string) => {
         const m = msg.toString()
         const idx = m.indexOf(' ')
 
         if (-1 !== idx) {
             const id = m.substring(0, idx)
             try {
-                const data:any = {}
-                //const data = bencode.decode(m.substring(parseInt(idx + 1)), 'utf8')
+                const data = bencode.decode(Buffer.from(m.substring(idx + 1)))
                 return { id, data }
             } catch (err) {
-                //console.error(err)
+                console.error(err)
             }
         }
     },
 
-    encodeMessage: function(id: string, data: any) {
+    encodeMessage: (id: string, data: any) => {
         return Buffer.from([id, bencode.encode(data)].join(' '))
     }
 
