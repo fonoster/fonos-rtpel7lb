e9c8d567cf74df13c89f4a2a6c07d2d11f33f775
Wardner <wanerlara1999@gmail.com>
2021-02-15 15:02:54 -0400

    Added consul list services function and unit tested

---
+++ src/consul/consul.ts
@@ -0,0 +1,9 @@
+// import Consul from 'consul'
+// const consul = new Consul();
+
+export const listServices = (err: any, result: any) => {
+  if (err) throw err
+  return result
+}
+
+// consul.agent.service.list(listServices);
