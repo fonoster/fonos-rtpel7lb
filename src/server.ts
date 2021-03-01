65d547825e4bf1eb450e357701ae1ab29e383c48
Wardner <wanerlara1999@gmail.com>
2021-03-01 13:46:09 -0400

    The socket is not working

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,79 @@
 import { Sockets } from './sockets/socket';
-import { RedisManager } from './redis/redisconnection';
+import { RedisManager } from './redis/redisconnection'
+const Client = require('rtpengine-client').Client
+const client = new Client()
 import Consul from 'consul'
-import { listServices } from './consul/consul';
 
 export default class RTPL {
+	private rtpList: object[] = []
+	private index: number = 0;
+	socket: Sockets;
+
     constructor(
-        private _Sockets: Sockets,
         private _RedisManager: RedisManager,
         private _Consul = new Consul()
-    ){}
+    )
+		{
+			// setInterval(this._Consul.agent.service.list(this.updateRtpList), 500)
+			this._Consul.agent.service.list((err, result) => this.updateRtpList(err, result))
+		}
 
     start() {
-        console.log('Hello world')
+      this.socket = new Sockets(this);
     }
 
-    updateRtpList() {
-      this._Consul.agent.service.list(listServices)
+    updateRtpList(err: any, result: any) {
+			if (err) throw err;
+			result = Object.values(result)
+			result.forEach((element: object) => {
+				// const exist = this.rtpList.filter((e: object) => e === element)
+				// if(exist) return
+				this.rtpList.push(element)
+			});
+			
+			// let uno = this.getNextRtpl()
+			// console.log(uno);
+			// for (let i = 0; i < result.length; i++) {
+			// 	const element: object = result[i];
+			// 	// this.rtpList.push(element)
+			// 	// console.log(element);
+			// }
+
+			
+			//rtpclient para conectarse al rtp
+			
+			// return console.log(result);
+
+			//Si es new agregar a la lista
+			//Si hay un valor que no viene y esta en la lista eliminar
+			//
+			//
+    }
+
+    public getNextRtpl(){
+			if(this.index == this.rtpList.length) this.index = 0
+			return this.rtpList[this.index++]
     }
 
     processRequest(callInfo: any) {
-        let obj = this._Sockets.onMessage(callInfo.msg, callInfo);
-        let call: string = this._RedisManager.get(obj.id)
+			console.log(callInfo);
+      let call: string = this._RedisManager.get(callInfo.id)
+			let rtpeInstance;
 
-        if(!call){
-            console.log('Es nueva');
-        } else {
-            console.log('Usar valor en Redis');
-        }
+      if(!call){
+        rtpeInstance = this.getNextRtpl();
+				// this._RedisManager.create(call, rtpeInstance.id)
+      } else {
+				rtpeInstance = this._RedisManager.get(call);
+			}
+			switch (callInfo.data.command){
+				case "ping": this.socket.reply(callInfo, 'pong')
+					break
+				case "offer":
+					break
+			}
 
-    }
-    
+			// Client(call, rtpeInstance);
+			console.log(rtpeInstance);
+  	}    
 }
--- src/sockets/socket.ts
+++ src/sockets/socket.ts
@@ -1,8 +1,18 @@
 import dgram from 'dgram'
 import { BencodeUtils } from '../utils/bencode_util'
-export const socket = dgram.createSocket('udp4')
+// export const socket = dgram.createSocket('udp4')
 
 export class Sockets {
+  socket: dgram.Socket;
+  server: any
+  constructor(
+    server: any 
+  ){
+    this.socket = dgram.createSocket('udp4')
+    this.socket.bind(8081)
+    this.socket.on('message', (value) => console.log(value))
+    this.server = server
+  }
 
   onMessage(msg: any, info: any) {
     let obj = BencodeUtils.decodeMessage(msg);
@@ -15,11 +25,23 @@ export class Sockets {
     console.log(`Received command '${obj.data.command}' from ${info.address}:${info.port}`);
     console.log(`Received command : `+JSON.stringify(obj));
 
-    return obj;
+    this.server.processRequest(obj);
+  }
+
+  
+  reply(info: any, message: any){
+    this.socket.send(message, info.port, info.address, function (error) {
+        if (error) {
+            console.log(`Error sending reply to client (${info.address}:${info.port}).`);
+        }
+    });
   }
 
 }
 
-const _socket = new Sockets();
-socket.on('message', _socket.onMessage.bind(this))
+
+
+// const _socket = new Sockets();
+// socket.on('message', _socket.onMessage.bind(this))
+// socket.bind(8081)
 
